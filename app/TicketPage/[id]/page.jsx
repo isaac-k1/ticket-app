9a7f733fd1c85793dbe857f59cae27fa800205fc
isaac-k1 <isaac.code980@gmail.com>
2024-01-08 15:31:38 -0600

    change api endpoints

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,7 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
 const getTicketById = async (id) => {
-  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+  const res = await fetch(`${process.env.BASE_URL}/api/Tickets/${id}`, {
     cache: "no-store",
   });
 
