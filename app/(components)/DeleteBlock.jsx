a5d557d40e1e9193fdc1d4fd5585169737f145ee
isaac-k1 <isaac.code980@gmail.com>
2024-01-08 15:37:27 -0600

    fix another api end point

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -7,7 +7,7 @@ const DeleteBlock = ({ id }) => {
   const router = useRouter();
 
   const deleteTicket = async () => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    const res = await fetch(`${process.env.BASE_URL}/api/Tickets/${id}`, {
       method: "DELETE",
     });
     if (res.ok) {
