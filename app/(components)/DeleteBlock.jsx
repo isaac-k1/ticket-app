dd9856c33571db589deea5285a9059a778759103
isaac-k1 <isaac.code980@gmail.com>
2024-01-08 16:47:14 -0600

    fix delete endpoint

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -7,7 +7,7 @@ const DeleteBlock = ({ id }) => {
   const router = useRouter();
 
   const deleteTicket = async () => {
-    const res = await fetch(`${process.env.BASE_URL}/api/Tickets/${id}`, {
+    const res = await fetch(`/api/Tickets/${id}`, {
       method: "DELETE",
     });
     if (res.ok) {
